099fa8d7c1e7a6dc5634dba448b61caf7a2fe711
Hugo Harada <hugo.harada@ifsp.edu.br>
2018-05-07 16:13:15 -0300

    Primeiro comentário

---
+++ main.c
@@ -0,0 +1,18 @@
+#
+# Exemplo de utilização de switch
+#
+# $_
+#
+
+#include <stdio.h>
+
+int main(){
+
+	printf("Escolha uma operação!!\n");
+	printf("0 - adição\n");
+	printf("1 - subtração\n"); 
+	printf("2 - multiplicação\n");
+	printf("3 - divisão\n");
+	return 0;
+
+}
