abeea0be91d975adf8b8ff94f347833101cc024f
Hugo Harada <hugo.harada@ifsp.edu.br>
2018-05-07 16:34:00 -0300

    adicao implementada

--- main.c
+++ main.c
@@ -1,18 +1,37 @@
-#
-# Exemplo de utilização de switch
-#
-# $_
-#
+//
+// Exemplo de utilização de switch
+//
 
 #include <stdio.h>
+#include "add.h"
 
 int main(){
-
+	int op, op1, op2;
 	printf("Escolha uma operação!!\n");
 	printf("0 - adição\n");
 	printf("1 - subtração\n"); 
 	printf("2 - multiplicação\n");
 	printf("3 - divisão\n");
+
+	fflush(stdin);
+	scanf("%d", &op);
+
+	printf("Entre dois operandos separados por um espaco!!\n");
+	fflush(stdin);
+	scanf("%d %d", &op1, &op2);
+
+
+	switch(op){
+		case 0:
+			printf("O resultado e: %d\n", add(op1,op2) ); //do addition
+			break;	
+		case 1: 
+			// do subtraction
+			break;
+		default:
+				printf("Argumento invalido\n");
+	}
+
 	return 0;
 
 }
